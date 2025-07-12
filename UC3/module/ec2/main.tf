9d678516c24df63465ab6e5d477ee28000709831
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-12 10:16:36 +0530

    Update main.tf

--- UC3/module/ec2/main.tf
+++ UC3/module/ec2/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2"{
     subnet_id = var.public_subnets[0]
     vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = true
-    user_data = file("${path.module}/userdata-0.sh")
+    user_data = file("${path.module}/userdata.sh")
     tags = {
       Name = "Instance-0"
     }
