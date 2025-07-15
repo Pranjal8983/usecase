7e652746ca71ec2d1014b243a3fec4ac8ea60f0d
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-15 17:23:03 +0530

    Delete usecase-5/s3 directory

--- usecase-5/s3/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "input_bucket_name" {
-  type        = string
-  description = "Input bucket name"
-}
-
-variable "output_bucket_name" {
-  type        = string
-  description = "Output bucket name"
-}
