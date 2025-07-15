e250cfeb3f31d5543a4bca663b6007f551f8ddd9
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-15 16:33:26 +0530

    Update variable.tf

--- usecase-5/s3/variable.tf
+++ usecase-5/s3/variable.tf
@@ -7,22 +7,3 @@ variable "output_bucket_name" {
   type        = string
   description = "Output bucket name"
 }
-
-variable "topic_arn" {
-  type = string
-}
-
-variable "events" {
-  type = list(string)
-  default = ["s3:ObjectCreated:*"]
-}
-
-variable "bucket_depends_on" {
-  type = any
-  default = []
-}
-
-variable "enable_event_notification" {
-  type = bool
-  default = false
-}
