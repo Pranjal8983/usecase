7f189d21c380bcf0dc025ef96dfb75a9cd730f6f
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-15 18:32:54 +0530

    Update variable.tf

--- usecase-6/variable.tf
+++ usecase-6/variable.tf
@@ -1,1 +1,9 @@
+variable "lambda_name" {
+  type        = string
+  description = "Lambda function name"
+}
 
+variable "lambda_role" {
+  type        = string
+  description = "Lambda IAM role arn"
+}
