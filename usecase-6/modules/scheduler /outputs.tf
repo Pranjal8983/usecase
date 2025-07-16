aa36de24d8f15184d16516b1c2fdddbbc40e5bc7
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-16 13:31:15 +0530

    Delete usecase-6/modules/scheduler  directory

--- usecase-6/modules/scheduler /outputs.tf
+++
@@ -1,3 +0,0 @@
-output "event_rule_arn" {
-  value = aws_cloudwatch_event_rule.this.arn
-}
--- usecase-6/modules/scheduler /variables.tf
+++
@@ -1,3 +0,0 @@
-variable "rule_name" {}
-variable "schedule_expression" {}
-variable "lambda_function_arn" {}
