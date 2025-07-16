49107ca1dae943d5abe5f67a83846c9c87256465
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-16 13:30:10 +0530

    Create variable.tf

---
+++ usecase-6/modules/event/variable.tf
@@ -0,0 +1,23 @@
+variable "name" {
+
+}
+
+variable "description" {
+
+}
+
+variable "schedule_expression" {
+
+}
+
+variable "lambda_name" {
+
+}
+
+variable "lambda_arn" {
+
+}
+
+variable "scheduler_role_arn" {
+
+}
