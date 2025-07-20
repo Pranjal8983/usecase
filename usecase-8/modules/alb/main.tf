3984405e08ad946f5125427445cad19ea11aec11
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-20 17:44:05 +0530

    Update main.tf

--- usecase-8/modules/alb/main.tf
+++ usecase-8/modules/alb/main.tf
@@ -18,6 +18,16 @@ resource "aws_lb_target_group" "patients" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "ip"
+
+  health_check {
+    path = "/health"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_target_group" "appointments" {
@@ -26,13 +36,23 @@ resource "aws_lb_target_group" "appointments" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "ip"
+
+    health_check {
+    path = "/health"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
 }
 
 # Listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
-  protocol          = "HTTP"
+  protocol         = "HTTP"
 
   default_action {
     type             = "fixed-response"
