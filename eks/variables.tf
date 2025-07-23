31c3a94c9cd544ced84230a54e75df73900f28af
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-23 10:53:44 +0530

    Create variables.tf

---
+++ eks/variables.tf
@@ -0,0 +1,14 @@
+variable "subnet_id_1" {
+  type = string
+  default = "subnet-0239f211447662e7a"
+ }
+
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-031ba2cbd22b52e60"
+ }
+
+ variable "cluster_name" {
+  type = string
+  default = "my-eks-cluster"
+}
