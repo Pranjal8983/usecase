1e9607a8fbd38045612c902deb752ff15a085c7f
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-23 11:29:51 +0530

    Update backend.tf

--- usecase-9/backend.tf
+++ usecase-9/backend.tf
@@ -1,1 +1,9 @@
-
+# Backend Configuration
+terraform {
+  backend "s3" {
+    bucket = "pranjal71025"
+    key    = "uc-01/terraform.tfstate"
+    use_lockfile = true
+    region = "us-east-1"
+  }
+}
