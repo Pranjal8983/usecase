cf04b4903c32fa5e35f6f535e670d9d69090d0ef
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-23 11:34:34 +0530

    Update variables.tf

--- usecase-9/variables.tf
+++ usecase-9/variables.tf
@@ -1,3 +1,9 @@
+#variable for the main.tf file
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
@@ -12,3 +18,4 @@ variable "subnet_id_2" {
   description = "Second subnet ID for EKS networking"
   type        = string
 }
+
