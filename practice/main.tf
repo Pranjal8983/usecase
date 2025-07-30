034a78dbf2e0de17640d46f9cf9846ed363dd99c
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-30 15:27:40 +0530

    Update main.tf

--- practice/main.tf
+++ practice/main.tf
@@ -3,7 +3,14 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-#===================
+#=========================
 resource "aws_security_group" "pranjal" {
   name        = "pranjal"
 }
+
+#==========================
+output "security_group_name" {
+  value       = aws_security_group.pranjal.name
+}
+#===========================
+
